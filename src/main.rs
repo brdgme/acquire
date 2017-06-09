83cb48907e5097e01db90ae06ade73c590640846
Michael <beefsack@gmail.com>
2017-06-09 22:35:10 +1000

    Changed main to repl

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 extern crate brdgme_cmd;
-
 extern crate acquire;
 
-use brdgme_cmd::cli::cli;
 use acquire::Game;
-use std::io::{stdin, stdout};
+use brdgme_cmd::repl;
 
 fn main() {
-    cli::<Game, _, _>(stdin(), &mut stdout());
+    repl::<Game>();
 }
